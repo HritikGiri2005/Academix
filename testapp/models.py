1169172ad9b6d9c3cdeaae3c547b5a1f4dc98ff1
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-13 22:39:20 +0530

    created User model for authentication

--- testapp/models.py
+++ testapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class User(AbstractUser): #for authentication
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
 
 class Student(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,related_name='Student')
