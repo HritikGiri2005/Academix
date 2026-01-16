2f4e641ac63bf6a5159b1d45ee3d2de712e84e17
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-16 21:34:44 +0530

    completed with models.py

--- testapp/models.py
+++ testapp/models.py
@@ -114,7 +114,19 @@ class ClassAssignment(models.Model):
     class Meta:
         ordering = ['-created_at']
 
-class SubmitAssignment(models)    
+class SubmitAssignment(models.Model):
+    student = models.ForeignKey(Student,related_name="student_submit",on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher,related_name="teacher_submit",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    submitted_assignment = models.ForeignKey(ClassAssignment,related_name='submission_for_assignment',on_delete=True)
+    submit = models.FileField(upload_to='Submission')
+
+    def __str__(self):
+        return "Submitted" +str(self.submitted_assignment.assignment_name)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
 
 
 
