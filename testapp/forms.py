1128bc9870fbd8ee4ac7d23b48b1978b1c5ee7fe
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-18 15:47:52 +0530

    created UserForm

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,2 +1,15 @@
 from django import forms
 from testapp.models import * 
+from django.contrib.auth.forms import UserCreationForm
+
+# user login form (Applied in both student and teacher login)
+class UserForm(UserCreationForm):
+    class Meta():
+        model = User
+        fields = ['username','password1','password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'answer'}),
+            'password1': forms.PasswordInput(attrs={'class':'answer'}),
+            'password2': forms.PasswordInput(attrs={'class':'answer'}),
+        }
+
