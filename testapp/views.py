1dd92e2291a7968fdcf245fd8ea5e9158a09a148
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-23 20:44:40 +0530

    imported all the necessary libraries

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-
+from django.views import generic
+from django.views.generic import View,TemplateView,ListView,DetailView,DeleteView,CreateView,UpdateView
 
 # Create your views here.
