d29667d9e5fe928e2919e098d49c1b8d4a2ba836
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-25 21:54:32 +0530

    started importing modules

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.views import generic
 from django.views.generic import View,TemplateView,ListView,DetailView,DeleteView,CreateView,UpdateView
+from django.utils.decorators import method_decorator
 
 # Create your views here.
