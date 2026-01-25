56e61399ab3f7779c5cd8827f7020f301cb21943
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-25 22:05:24 +0530

    incomplete teacher signup

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views import generic
 from django.views.generic import View,TemplateView,ListView,DetailView,DeleteView,CreateView,UpdateView
 from django.utils.decorators import method_decorator
+from testapp.forms import UserForm,TeacherProfileForm
 
 # Create your views here.
 
+
+#teacher signup 
+def TeacherSignUp(request):
+    user_type = 'teacher'
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data = request.POST)
+        teacher_profile_form = TeacherProfileForm(data = request.POST)
+        
+         
 
