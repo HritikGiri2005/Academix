04581409e8a53b6d62adf49fa121f2b75566fd8c
HritikGiri2005 <hritikgiri2@gmail.com>
2026-03-03 21:48:22 +0530

    changed project level urls

--- Academix/urls.py
+++ Academix/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.HomePage.as_view(),name = "home"),
-    
-]
+    path('',views.HomePage.as_view(),name="home"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('classroom/',include('testapp.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
